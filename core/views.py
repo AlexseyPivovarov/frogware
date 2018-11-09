5878d41472e6407e2b5c592b91e17abb959b509a
AlexseyPivovarov <a.peevovarov@gmail.com>
2018-11-09 21:00:40 +0200

    upload

---
+++ core/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, HttpResponse
+from django.views import View
+from json import loads
+
+
+# Create your views here.
+class MyMeinView(View):
+
+    def get(self, request):
+        with open('json/MainPageInfo.json', encoding='utf-8', mode='r') as main_js:
+            context = {}
+            context['main'] = loads(main_js.read())
+            return render(request, template_name='main.html', context=context)
+
+
+class MyDetailView(View):
+
+    def get(self, request, alias, globalId):
+        context = {}
+        context['finished'] = []
+        with open('json/GeneralQuestsInfo.json', encoding='utf-8', mode='r') as general_js:
+            for item in loads(general_js.read()):
+                if item['alias'] == alias and item['globalId'] == globalId:
+                    context['general'] = item
+                    break
+            else:
+                return render(request, template_name='not-found.html')
+        with open('json/FinishedQuestsLeafs.json', encoding='utf-8', mode='r') as finished_js:
+            for item in loads(finished_js.read()):
+                if item['questId'] == context['general']['id']:
+                    context['finished'].append(item['name'])
+        return render(request, template_name='detail.html', context=context)
